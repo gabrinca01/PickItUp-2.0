ce16d46d14f917a85a9b169f3516c3228376200b
redaster3 <redaster3@gmail.com>
2023-03-30 19:10:13 +0200

    rifiniture css

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,7 @@ class UserTest < ActiveSupport::TestCase
 
   def setup
     @user=User.new(id: 1, email: "redaster@gmail.com", created_at: "2023-03-28 18:47:16.138934000 +0000", updated_at: "2023-03-28 18:47:43.050252000 +0000", profile_picture: nil, accumulated_points: nil, points: nil, username: "redaster",
-       provider: nil, uid: nil, role: "level0", country_code: nil, verified: true, num_tel: "+393337991834")
+       provider: nil, uid: nil, password:"passprova" role: "level0", country_code: nil, verified: true, num_tel: "+393337991834")
   end
 
 
